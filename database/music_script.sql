0ae9b9eec2149c817545b7611a72d6bc0dce6ce9
Stanley <stanleyhuang0702@gmail.com>
2025-12-31 13:45:28 -0500

    update database script

--- database/music_script.sql
+++ database/music_script.sql
@@ -8,13 +8,12 @@ CREATE DATABASE `Songs_Database`;
 USE `Songs_Database`;
 
 CREATE TABLE songs (
-    id BIGINT NOT NULL AUTO_INCREMENT,
+    id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     artist VARCHAR(50),
     song_name VARCHAR(50),
     song_file_name VARCHAR(50),
     /* duration is in seconds */
-    duration INT,
-    PRIMARY KEY (id)
+    duration INT
 );
 
 INSERT INTO songs (artist, song_name, song_file_name, duration) VALUES
