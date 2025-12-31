e4ec35ffb91a226b6337aca023b5804cace9f01e
Stanley <stanleyhuang0702@gmail.com>
2025-12-30 20:00:56 -0500

    add database script

---
+++ database/music_script.sql
@@ -0,0 +1,21 @@
+/* 
+SCRIPT IS FOR MySQL
+Generate tables and data for the program 
+*/
+
+DROP DATABASE IF EXISTS `Songs_Database`;
+CREATE DATABASE `Songs_Database`;
+USE `Songs_Database`;
+
+CREATE TABLE songs (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    artist VARCHAR(50),
+    song_name VARCHAR(50),
+    song_file_name VARCHAR(50),
+    /* duration is in seconds */
+    duration INT,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO songs (artist, song_name, song_file_name, duration) VALUES
+('Yorushika', 'Nautilus', 'nautilus.mp4', 262);
