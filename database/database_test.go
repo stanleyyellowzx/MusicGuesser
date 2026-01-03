4d828059d2298323205f81807d30aedcc3bdd207
Stanley <stanleyhuang0702@gmail.com>
2026-01-03 13:06:11 -0500

    used testing Log function instead of fmt.Println

--- database/database_test.go
+++ database/database_test.go
@@ -2,23 +2,21 @@ package database
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestNoConnectionToDatabase(t *testing.T) {
-	fmt.Println("test.go file")
-	fmt.Println("testing GetAllSongData before connection to database")
+	t.Log("testing GetAllSongData before connection to database")
 	GetAllSongData()
-	fmt.Println("testing GetArtistSongData before connection to database")
+	t.Log("testing GetArtistSongData before connection to database")
 	GetArtistSongData("Yorushika")
 }
 
 func TestConnectionToDatabase(t *testing.T) {
-	fmt.Println("connecting to database")
+	t.Log("connecting to database")
 	ConnectToDatabase()
-	fmt.Println("testing GetAllSongData after connection to database")
+	t.Log("testing GetAllSongData after connection to database")
 	GetAllSongData()
-	fmt.Println("testing GetArtistSongData after connection to database")
+	t.Log("testing GetArtistSongData after connection to database")
 	GetArtistSongData("Yorushika")
 	CloseDatabase()
 }
